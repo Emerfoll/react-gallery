59a9aed7022ebeef14f4b2ea6fb014b2df453f89
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2021-01-17 14:39:49 -0600

    Updated pics and descriptions.

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,14 +1,13 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0, clicked: 0 },
-    { id: 2, path: 'images/Brii&Me.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0, clicked: 0 },
-    { id: 3, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0, clicked: 0 },
-    { id: 4, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0, clicked: 0 },
-    { id: 5, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0, clicked: 0 },
-    { id: 6, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0, clicked: 0 },
-    { id: 7, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0, clicked: 0 },
-    { id: 8, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0, clicked: 0 },
-    { id: 9, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0, clicked: 0 },
-    { id: 10, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0, clicked: 0 }
+    { id: 1, path: 'images/Brii&Me.jpg', description: 'Photo of my room mate and I at a wedding.', likes: 0, clicked: 0 },
+    { id: 2, path: 'images/Soul.jpg', description: 'Photo of my cat Soul taking a nap under the covers.', likes: 0, clicked: 0 },
+    { id: 3, path: 'images/Kevin.jpg', description: 'Photo of my dog Kevin taking a nap.', likes: 0, clicked: 0 },
+    { id: 4, path: 'images/AnimeSoul.jpg', description: 'Photo of Soul with his big anime eyes #nofilter.', likes: 0, clicked: 0 },
+    { id: 5, path: 'images/BestBuds.png', description: 'Photo of Kevin and Soul chillin together.', likes: 0, clicked: 0 },
+    { id: 6, path: 'images/KevinSoul.jpg', description: 'Photo of Kevin and Soul napping together.', likes: 0, clicked: 0 },
+    { id: 7, path: 'images/KevinSoul2.jpg', description: 'Another photo of Kevin and Soul napping together.', likes: 0, clicked: 0 },
+    { id: 8, path: 'images/KevSoul.jpg', description: 'Photo of Kevin and Soul just hanging out on the couch.', likes: 0, clicked: 0 },
+    { id: 9, path: 'images/Paddleboard.jpg', description: 'Photo of Cedar Lake, one of my favorite paddle boarding spots.', likes: 0, clicked: 0 }
 ];
 
 module.exports = galleryItems;
